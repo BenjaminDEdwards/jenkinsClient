a62124ae56acabeeb2dcd29bba29e0e3f9b076d9
Ben Edwards <ben@ben.com>
2024-09-27 13:34:06 +0100

    lambda

--- Jenkins/Client.py
+++ Jenkins/Client.py
@@ -1,11 +1,17 @@
 import requests
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, TypeVar, Callable, Dict
 
 from requests.auth import HTTPBasicAuth
 
 @dataclass(frozen=True)
 class JenkinsJob:
+  url: str
+  buildable: bool
+  next_build_number: int
+
+@dataclass(frozen=True)
+class JenkinsQueueItem:
   url: str
   buildable: bool
   next_build_number: int
@@ -16,15 +22,46 @@ class RestClient:
   password: str
   base_url: str
 
-  def getJenkinsJob(self, job_name: str) -> Optional[JenkinsJob]
-    url = f"{self.base_url}/job/{job_name}/api/json"
+  def getJenkinsJob(self, job_name: str) -> Optional[JenkinsJob]:
+    return self.__apiCall(
+      f"/job/{job_name}",
+      lambda json: JenkinsJob(
+        url=json["url"],
+        buildable=json["buildable"],
+        next_build_number=json["nextBuildNumber"],
+        in_queue=json[inQueue]
+      )
+    )
+
+
+  def getQueueItem(self, queue_id: int) -> Optional[JenkinsQueueItem]:
+    return self.__apiCall(
+      f"/queue/item/{queue_id}",
+      lambda json: JenkinsQueueItem(
+        url = json["url"],
+        buildable = json["buildable"],
+      )
+    )
+
+    url = f"{self.base_url}/queue/item/{queue_id}/api/json"
     response = requests.get(url, auth=HTTPBasicAuth(self.username, self.password))
     if response.status_code == 200:
-      return JenkinsJob(
+      return JenkinsQueueItem(
         url=response.json()["url"],
         buildable=response.json()["buildable"],
-        next_build_number=response.json()["nextBuildNumber"]
+        next_build_number=response.json()["nextBuildNumber"],
+        in_queue=response.json()["inQueue"]
       )
     else:
-      print(f"Failed to get Jenkins job {job_name}. Status code: {response.status_code}")
+      print(f"Failed to get Jenkins queue item {queue_id}. Status code: {response.status_code}")
+      return None
+
+  A = TypeVar("A")
+  def __apiCall(self, path: str, transform: Callable[[Dict],A]) -> Optional[A]:
+    url = f"{self.base_url}{path}/api/json"
+    response = requests.get(url, auth=HTTPBasicAuth(self.username, self.password))
+    if response.status_code == 200:
+      return transform( response.json() )
+    else:
+      print(f"Failed to make REST call to {url}. Status code: {response.status_code}")
       return None
